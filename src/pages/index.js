43951c08b1be7892442ca5c9d976550f0616258f
MariaCorin25 <maria.corin+6B@estudante.ifms.edu.br>
2024-10-07 15:41:30 +0000

    aula 07.10

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,21 @@
 import Footer from "./component/footer";
 import Navbar from "./component/navbar";
+import { Container, Row, Col} from "react-bootstrap";
+
 export default function Index(){
-  return
-  <>
+  return <>
   <Navbar />
-  <h1>Olá Mundo</h1>
+  <Container>
+      <Row>
+        <Col className="bg-primary">1 of 2</Col>
+        <Col className="bg-success">2 of 2</Col>
+      </Row>
+      <Row>
+        <Col className="bg-danger">1 of 3</Col>
+        <Col className="bg-warning">2 of 3</Col>
+        <Col className="grad">3 of 3</Col>
+      </Row>
+    </Container>
   <Footer />
    </>
 }
